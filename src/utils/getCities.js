fc0f1d205ecefa6b1baee2d3b1ea15993a4df3b8
Zach <104291766+zachary-o@users.noreply.github.com>
2023-05-14 19:22:18 +0300

    add BASE_URL and api key

--- src/utils/getCities.js
+++ src/utils/getCities.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
+const BASE_URL = " https://api.weatherapi.com/v1";
+
 export const getCities = async (cityName) => {
   if (!cityName) {
     return []
   }
   const response = await axios
     .get(
-      `https://api.weatherapi.com/v1/search.json?key=d22b133886504c7584b174852232604&q=${cityName}`
+      `${BASE_URL}/search.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${cityName}`
     )
     .catch((error) => console.log(error));
 
--- src/utils/getCityWeather.js
+++ src/utils/getCityWeather.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+const BASE_URL = " https://api.weatherapi.com/v1";
+
+
 export const getCityWeather = async (searchCity) => {
   const response = await axios.get(
-    `http://api.weatherapi.com/v1/forecast.json?key=d22b133886504c7584b174852232604&q=${searchCity}&days=1&aqi=no&alerts=no`
+    `${BASE_URL}/forecast.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${searchCity}&days=1&aqi=no&alerts=no`
   );
 
   const cityWeather = response.data;
