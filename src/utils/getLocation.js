bcb1bf8716a20eb517ae62c5e7d7e2521039dd40
Zach <104291766+zachary-o@users.noreply.github.com>
2023-05-15 15:42:18 +0300

    fix the getLocation function

--- src/utils/getLocation.js
+++ src/utils/getLocation.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export const getLocation = async () => {
   const location = await axios.get("https://ipapi.co/json");
+  console.log(location.data.city);
   return location.data;
 };
