8da3803285b88ebce48817831c8de7760dad40c3
Zach <104291766+zachary-o@users.noreply.github.com>
2023-06-10 00:34:38 +0300

    bug fix

--- src/utils/getCityWeather.js
+++ src/utils/getCityWeather.js
@@ -3,7 +3,9 @@ import axios from "axios";
 const BASE_URL = " https://api.weatherapi.com/v1";
 
 export const getCityWeather = async (searchCity) => {
-  
+  if (searchCity === "Kyiv") {
+    searchCity = "Kiev";
+  }
   const response = await axios.get(
     `${BASE_URL}/forecast.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${searchCity}&days=2&aqi=no&alerts=no`
   );
