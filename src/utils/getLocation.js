17d182b6c501a1ced8dd8f78d7c4c14ad9a8fb8f
Zach <104291766+zachary-o@users.noreply.github.com>
2023-07-12 23:29:22 +0700

    responsive CSS

--- src/utils/getLocation.js
+++ src/utils/getLocation.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const getLocation = async () => {
-  const response = await axios.get("https://ipapi.co/json");
-  
+  const response = await axios.get("http://ip-api.com/json");
+
   const myLocation = response.data.city;
   return myLocation;
 };
